ddba17d9820d7adc01e60014242eccf876fc5dc2
jmadondo1722 <jmadondo1722@gmail.com>
2022-10-06 06:01:54 +0200

    Added my methods to the repository. Not sure if they work but will discuss in the meeting

--- hilogame.py
+++ hilogame.py
@@ -1,30 +1,69 @@
-
 class game:
 
 #Methods 1
 
-    def _init_(self):
+    def __init__(self):
+        """Initiates a new game with is_playing and point_value as attributes"""
         self.is_playing = True
-        self.point_value = 400
+        self.point_value = 300
+        self.current_card = ""
+        self.higher = ""
+        self.lower = ""
+        self.choice = 0
+        self.score = 0
+        self.total_score = 0
+        
 #Methods 2-4
-#
+    """These functions will go through a round of the game. """
+    def start_round(self):
+
+        pass
+
+#Method 3
+    def input(self):
+        """Ask the user if the next card is higher or lower. (Josiah)"""
+        self.current_card = ""
+        self.next_card = input("Is next next higher or lower?")
+        self.is_playing = (self.next_card == "y")
+        if self.is_playing == self.higher:
+            self.choice = 1
+        if self.is_playing == self.lower:
+            self.choice = 2
+        return self.choice
+
+    #Method 4
+    def update_points(self):
+        """Updates the player's score.(Josiah)"""
+        if not self.is_playing:
+            return
+
+        for i in range(len(self.value)):
+            card = self.value[i]
+            card.generate()
+            self.score += card.points 
+        self.total_score += self.score
+
 #Method 5
-    def do_output(self):
+    def output(self):
+        """Displays the score and will evaluate if the user is able to or wants to keep playing.
+        Arguments: 
+        """
         if not self.is_playing:
             return
         elif self.point_value > 0:
-            print("Would you like to keep going?")
+            print(f"Your score is {self.point_value}. Would you like to keep going?")
             self.is_playing = input("y/n: ")
             if self.is_playing == "n":
-                pass
-                #run game end method, which will display score and run initialize game
+                self.end_game()
+            elif self.is_playing == "y":
+                self.start_round()
         elif self.point_value < 0:
-            #display score
-            #Run game init
-            pass
+            print("You have no more points to play")
+            self.end_game()
 
 #Method 6
     def end_game(self):
-        pass
-
-
+        while not self.is_playing:
+            print(f"Your score was {self.point_value}")
+            self.point_value == 300
+            self.is_playing = True
